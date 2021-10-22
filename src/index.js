2b5091a97f71c3ea6cbd1ee3a3ce70ebcea89ec0
Subham <subhamy142@gmail.com>
2021-10-22 09:27:05 +0530

    added pipes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const config = {
     // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
-      gravity: { y: 400 },
       debug: true,
     }
   },
@@ -20,27 +19,45 @@ const config = {
   }
 }
 
+let bird = null;
+let upperPipe=null;
+let lowerPipe = null;
+const pipeVerticalDistanceRange = [150, 250];
+let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
+const flapVelocity = 250;
+const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
+
+
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png')
+  this.load.image('pipe', 'assets/pipe.png')
 }
 
 const VELOCITY = 200;
 
-let bird = null;
-let flapVelocity = 250;
 
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
+  bird.body.gravity.y=400;
+  upperPipe = this.physics.add.sprite(400,100,'pipe').setOrigin(0,1);
+  lowerPipe = this.physics.add.sprite(400,upperPipe.y+pipeVerticalDistance,'pipe').setOrigin(0,0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 function update(time, delta) {
-
+if(bird.y >config.height || bird.y <  -bird.height){
+   restartBirdPosition();
+}
+}
+function restartBirdPosition(){
+  bird.x=initalBirdPosition.x;
+  bird.y=initalBirdPosition.y;
+  bird.body.velocity.y =0;
 }
 
 function flap() {
