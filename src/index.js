6ec3bfc8bab84beb6d21006138e076494f956943
Subham <subhamy142@gmail.com>
2021-10-28 16:20:17 +0530

    added multiple pipes

--- src/index.js
+++ src/index.js
@@ -18,12 +18,13 @@ const config = {
     update
   }
 }
-
+const PIPES_TO_RENDER = 4;
 let bird = null;
 let upperPipe=null;
 let lowerPipe = null;
+let pipeHorizontalDistance = 0;
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
+
 const flapVelocity = 250;
 const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
 
@@ -42,9 +43,17 @@ function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
   bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y=400;
-  upperPipe = this.physics.add.sprite(400,100,'pipe').setOrigin(0,1);
-  lowerPipe = this.physics.add.sprite(400,upperPipe.y+pipeVerticalDistance,'pipe').setOrigin(0,0);
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
 }
