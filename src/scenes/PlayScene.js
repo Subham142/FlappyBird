509ee3a7b838a9b403d74f559f7f8ad51bafd2da
Subham <subhamy142@gmail.com>
2021-11-18 00:22:17 +0530

    added score and best score

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -23,6 +23,7 @@ class PlayScene extends Phaser.Scene {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('bird', 'assets/bird.png');
     this.load.image('pipe', 'assets/pipe.png');
+    this.load.image('pause', 'assets/pause.png');
   }
 
   create() {
@@ -31,6 +32,7 @@ class PlayScene extends Phaser.Scene {
     this.createPipes();
     this.createColliders();
     this.createScore();
+    this.createPause();
     this.handleInputs();
   }
 
@@ -68,7 +70,21 @@ class PlayScene extends Phaser.Scene {
 
   createScore() {
     this.score = 0;
+    const bestScore = localStorage.getItem('bestScore');
     this.scoreText = this.add.text(16, 16, `Score: ${0}`, { fontSize: '32px', fill: '#000'});
+    this.add.text(16, 52, `Best score: ${bestScore || 0}`, { fontSize: '18px', fill: '#000'});
+  }
+
+  createPause() {
+    const pauseButton = this.add.image(this.config.width - 10, this.config.height -10, 'pause')
+    .setInteractive()
+    .setScale(3)
+    .setOrigin(1);
+
+  pauseButton.on('pointerdown', () => {
+    this.physics.pause();
+    this.scene.pause();
+  })
   }
 
   handleInputs() {
@@ -103,6 +119,7 @@ class PlayScene extends Phaser.Scene {
         if (tempPipes.length === 2) {
           this.placePipe(...tempPipes);
           this.increaseScore();
+          this.saveBestScore();
         }
       }
     })
@@ -116,6 +133,15 @@ class PlayScene extends Phaser.Scene {
     })
 
     return rightMostX;
+  }
+
+  saveBestScore() {
+    const bestScoreText = localStorage.getItem('bestScore');
+    const bestScore = bestScoreText && parseInt(bestScoreText, 10);
+
+    if (!bestScore || this.score > bestScore) {
+      localStorage.setItem('bestScore', this.score);
+    }
   }
 
   gameOver() {
