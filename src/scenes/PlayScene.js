1f15a70321c5c0a5fa82f76be822c6100ba8a2ef
Subham <subhamy142@gmail.com>
2021-11-18 18:04:46 +0530

    added resume feature

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -9,6 +9,7 @@ class PlayScene extends BaseScene {
 
     this.bird = null;
     this.pipes = null;
+    this.isPaused = false;
 
     this.pipeHorizontalDistance = 0;
     this.pipeVerticalDistanceRange = [150, 250];
@@ -27,11 +28,38 @@ class PlayScene extends BaseScene {
     this.createScore();
     this.createPause();
     this.handleInputs();
+    this.listenToEvents();
   }
 
   update() {
     this.checkGameStatus();
     this.recyclePipes();
+  }
+
+  listenToEvents() {
+    if (this.pauseEvent) { return; }
+
+    this.pauseEvent = this.events.on('resume', () => {
+      this.initialTime = 3;
+      this.countDownText = this.add.text(...this.screenCenter, 'Fly in: ' + this.initialTime, this.fontOptions).setOrigin(0.5);
+      this.timedEvent = this.time.addEvent({
+        delay: 1000,
+        callback: this.countDown,
+        callbackScope: this,
+        loop: true
+      })
+    })
+  }
+
+  countDown() {
+    this.initialTime--;
+    this.countDownText.setText('Fly in: ' + this.initialTime);
+    if (this.initialTime <= 0) {
+      this.isPaused = false;
+      this.countDownText.setText('');
+      this.physics.resume();
+      this.timedEvent.remove();
+    }
   }
 
   createBG() {
@@ -69,12 +97,14 @@ class PlayScene extends BaseScene {
   }
 
   createPause() {
+    this.isPaused = false;
     const pauseButton = this.add.image(this.config.width - 10, this.config.height -10, 'pause')
     .setInteractive()
     .setScale(3)
     .setOrigin(1);
 
   pauseButton.on('pointerdown', () => {
+    this.isPaused = true;
     this.physics.pause();
     this.scene.pause();
 
@@ -155,6 +185,7 @@ class PlayScene extends BaseScene {
   }
 
   flap() {
+    if (this.isPaused) { return; }
     this.bird.body.velocity.y = -this.flapVelocity;
   }
 
